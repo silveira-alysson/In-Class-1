f068d47f7f1bdd0b2f9462386b975d9524f4655a
Alysson Silveira <alysson.osilveira@gmail.com>
2019-09-10 20:58:57 -0400

    change

---
+++ Week_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Week_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years of experience do you have in professional programming?");
+            string input = Console.ReadLine();
+            int xp = int.Parse(input);
+            int i = 0;
+
+            while ((i >= 0) && (i <= xp))
+            {
+                Console.WriteLine("You have " + i + "years of experience");
+                i = i + 1;
+            }
+
+        }
+    }
+}
